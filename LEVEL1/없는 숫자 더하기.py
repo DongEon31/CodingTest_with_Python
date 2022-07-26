c7734103e11c1d509953feb554f824dcc515c566
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-07-26 21:31:59 +0900

    Create 없는 숫자 더하기.py

---
+++ LEVEL1/없는 숫자 더하기.py
@@ -0,0 +1,12 @@
+## 없는 숫자 더하기 - 프로그래머스 월간 코드 챌린지 시즌3 
+## https://school.programmers.co.kr/learn/courses/30/lessons/86051
+
+def solution(numbers):
+    answer = 0
+    num_list = [0,1,2,3,4,5,6,7,8,9]
+    my_list = []
+    for number in numbers:
+        my_list.append(number)
+    answer = sum((list(set(num_list) - set(my_list))))
+    return answer
+    
