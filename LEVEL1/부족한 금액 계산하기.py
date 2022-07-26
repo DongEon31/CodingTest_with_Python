cf1adb232e945a86ee44ed22792bd3e8912317f4
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-07-26 21:35:14 +0900

    Create 부족한 금액 계산하기.py

---
+++ LEVEL1/부족한 금액 계산하기.py
@@ -0,0 +1,16 @@
+## 부족한 금액 계산하기 - 위클리 챌린지
+## https://school.programmers.co.kr/learn/courses/30/lessons/82612
+
+def solution(price, money, count):
+    answer = -1
+    total = 0
+    for n in range(count):
+        new_price = price * (n+1)
+        total += new_price
+    if total > money :
+        answer = total - money
+    else :
+        answer = 0
+    
+    return answer
+  
