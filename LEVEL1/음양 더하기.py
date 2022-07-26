6016124d8b2bfbfd48bb7c97df1854f6cfe9f841
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-07-26 21:35:57 +0900

    Create 음양 더하기.py

---
+++ LEVEL1/음양 더하기.py
@@ -0,0 +1,12 @@
+## 음양 더하기 - 월간 코드 챌린지 시즌2
+## https://school.programmers.co.kr/learn/courses/30/lessons/76501
+
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True : t = 1
+        else : t = -1
+        absolutes[i] *= t
+        answer +=  absolutes[i]
+    return answer
+  
