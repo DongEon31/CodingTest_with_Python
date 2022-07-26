3375442b286936554eedd5965344815dbc15f4e2
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-07-26 21:44:03 +0900

    직사각형 별찍기

--- LEVEL1/직사각형 별찍기.py
+++ LEVEL1/직사각형 별찍기.py
@@ -1,6 +1,6 @@
 ## 직사각형 별찍기
 ## https://school.programmers.co.kr/learn/courses/30/lessons/12969
-
+ 
 a, b = map(int, input().strip().split(' '))
 for _ in range(b):
     list = ['*' for i in range(a)]
