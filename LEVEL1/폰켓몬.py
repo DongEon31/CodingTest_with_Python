0c98230fd08d559570ee93bac70a9661c6f48e36
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-07-26 21:44:43 +0900

    폰켓몬

--- LEVEL1/폰켓몬.py
+++ LEVEL1/폰켓몬.py
@@ -5,7 +5,7 @@ def solution(nums):
     list = []
     # list인 nums를 set로 만드는 과정인데, 그냥 set(nums)를 써도 됨
     for num in nums:
-        if num not in list :
+        if num not in list : 
             list.append(num)
  
     return min(len(nums)/2,len(list))
