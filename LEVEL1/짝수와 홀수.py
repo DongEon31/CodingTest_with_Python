df0d960113918612284e7603dbbd574d5ff97d07
DongEon31 <90170785+DongEon31@users.noreply.github.com>
2022-08-27 13:51:01 +0900

    짝수와 홀수

---
+++ LEVEL1/짝수와 홀수.py
@@ -0,0 +1,9 @@
+## 짝수와 홀수
+## https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+def solution(num):
+    answer = ''
+    answer += 'Odd' if num % 2 == 1 else 'Even'
+    return answer
+  
+  
